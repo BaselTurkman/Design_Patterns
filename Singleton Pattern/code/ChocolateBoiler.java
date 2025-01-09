5b7492f94a3507a8f63e4dbde6523e5c5d6dd9bf
Basel Turkman <140549736+BaselTurkman@users.noreply.github.com>
2025-01-10 00:31:59 +0200

    Add files via upload

--- Singleton Pattern/code/ChocolateBoiler.java
+++ Singleton Pattern/code/ChocolateBoiler.java
@@ -1,16 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.chocolateboiler;
 
-/**
- *
- * @author HITECH
- */
 public class ChocolateBoiler {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private boolean empty;
+    private boolean boiled;
+    private static ChocolateBoiler uniqueInstance;
+
+    //ii. Eager Instantiation
+//    private static final ChocolateBoiler uniqueInstance = new ChocolateBoiler();
+//
+//    public static ChocolateBoiler getInstance() {
+//        return uniqueInstance;
+//    }
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    //a. Lazy Instantiation Singleton (Single-threaded)
+    public static ChocolateBoiler getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
+    }
+
+//    //b. Thread-Safe Singleton Implementations
+//    //i. Synchronization
+//    public static synchronized ChocolateBoiler getInstance() {
+//        if (uniqueInstance == null) {
+//            uniqueInstance = new ChocolateBoiler();
+//        }
+//        return uniqueInstance;
+//    }
+    
+    //iii. Double-Checked Locking
+//    public static ChocolateBoiler getInstance() {
+//        if (uniqueInstance == null) {
+//            synchronized (ChocolateBoiler.class) {
+//                if (uniqueInstance == null) {
+//                    uniqueInstance = new ChocolateBoiler();
+//                }
+//            }
+//        }
+//        return uniqueInstance;
+//    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
     }
 }
